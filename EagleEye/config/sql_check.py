1ad15411181cfd3bfd56b4074dba1d233eec0cca
wang.zy <wang.zy@Ctrip.com>
2016-01-08 18:09:26 +0800

    剥离推荐查询到单独SQL中，便于管理

--- EagleEye/config/sql_check.py
+++ EagleEye/config/sql_check.py
@@ -44,55 +44,7 @@ FROM
 WHERE
     CreateDate >= curdate()
         """
-# 推荐接口-次数
-ShoppingService_RecommendSearch_Times = """
-SELECT
-    ServiceInvokeId, DataChange_LastTime
-FROM
-    monitor.serviceinvokelog
-WHERE
-    DataChange_LastTime >= %(sdt)s
-        AND DataChange_LastTime <= %(edt)s
-        and productTypeName = 'ShoppingService_RecommendSearch'
-        and productType = (case when productType=-1 then productType else %(producttype) end)
-        and isIntlflight = (case when isIntlflight = -1 then  isIntlflight else %(isintlflight) end)
-        and isIntlHotel = (case when isIntlHotel = -1 then isIntlHotel else %(isintlhotel) end )
-        and channel = (case when channel =-1 then channel else %(channel) end);
-"""
-# 推荐接口-次数
-ShoppingService_RecommendSearch_Failure_Times = """
-SELECT
-    ServiceInvokeId, DataChange_LastTime
-FROM
-    monitor.serviceinvokelog
-WHERE
-    DataChange_LastTime >= %(sdt)s
-        AND DataChange_LastTime <= %(edt)s
-        and productTypeName = 'ShoppingService_RecommendSearch'
-        and invokewstatus in (2,3,-1)
-        and productType = (case when productType=-1 then productType else %(producttype) end)
-        and isIntlflight = (case when isIntlflight = -1 then  isIntlflight else %(isIntlflight) end)
-        and isIntlHotel = (case when isIntlHotel = -1 then isIntlHotel else %(isIntlHotel) end )
-        and channel = (case when channel =-1 then channel else %(channel) end)
-        ;
-"""
 
-# 推荐接口-耗时
-ShoppingService_RecommendSearch_Elapsed = """
-SELECT
-    ServiceInvokeId,elapsedtime, DataChange_LastTime
-FROM
-    monitor.serviceinvokelog
-WHERE
-    DataChange_LastTime >= %(sdt)s
-        AND DataChange_LastTime <= %(edt)s
-        and productTypeName = 'ShoppingService_RecommendSearch'
-        and productType = (case when productType=-1 then productType else %(producttype) end)
-        and isIntlflight = (case when isIntlflight = -1 then  isIntlflight else %(isIntlflight) end)
-        and isIntlHotel = (case when isIntlHotel = -1 then isIntlHotel else %(isIntlHotel) end )
-        and channel = (case when channel =-1 then channel else %(channel) end)
-        and elapsedtime > 0;
-        """
 # -- CheckAvailableLogDetail
 # -- CheckAvailableLogDetailID	自增主键
 # -- CheckAvailableID	主表Id
