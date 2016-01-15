ac293813a1a380d68c9b1dbf5f249f1e49fdbe46
wang.zy <wang.zy@Ctrip.com>
2016-01-15 11:32:35 +0800

    去除登陆重定向，没有必要，故删除掉

--- Monitor/urls.py
+++ Monitor/urls.py
@@ -6,9 +6,9 @@ urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'Monitor.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-
+                       url(r'^$', include('EagleEye.urls')),
                        url(r'^EagleEye/', include('EagleEye.urls')),
-                       url(r'^$', views_base.to_dashboard, name='dashboard'),
+                       # url(r'^$', views_base.to_dashboard, name='dashboard'),
                        url(r'^login/?$', 'django_cas_ng.views.login'),
                        url(r'^logout/$', 'django_cas_ng.views.logout'),
                        url(r'^admin/', include(admin.site.urls)),
