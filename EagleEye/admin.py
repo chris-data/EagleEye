f090a5f4c9a9efbe6a5e573068646051b98b807a
wang.zy <wang.zy@Ctrip.com>
2016-01-20 22:29:12 +0800

    add rights

--- EagleEye/admin.py
+++ EagleEye/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from EagleEye.models import DiyPageviewStatisticRealtime,predictedorders,DiyOrderRealtime,CommitOrder,BookingOrder
+
+admin.site.register(DiyPageviewStatisticRealtime)
+admin.site.register(predictedorders)
+admin.site.register(DiyOrderRealtime)
+admin.site.register(CommitOrder)
+admin.site.register(BookingOrder)
